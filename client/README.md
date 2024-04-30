cddf2eb07851f7d82f4b82e2ff4ca22f557e2fed
Omkar Pomendkar <omkarpomendkar100@gmail.com>
2024-04-30 13:17:22 +0530

    create pages and route

--- client/README.md
+++ client/README.md
@@ -1,4 +1,6 @@
 1) Created vite Project 
 2) Added Tailwind with vite react
 3) Create a github Repository
-`
+4) npm i react-router-dom
+5) Create pages and Routes
+6) 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import {BrowserRouter , Routes ,Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
 
 const App = () => {
   return (
-    <div>
-      <h1 className=' text-5xl bg-yellow-300'> Hello Guys</h1>
-    </div>
-  ) 
+    <BrowserRouter>
+      <Routes>
+        <Route path= "/" element={<Home/>}/>
+        <Route path= "/about" element={<About/>}/>
+        <Route path= "/sign-in" element={<Signin/>}/>
+        <Route path= "/sign-up" element={<Signup/>}/>
+        <Route path= "/dashboard" element={<Dashboard/>}/>
+        <Route path= "/projects" element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
+  )              
 }
 
 export default App
